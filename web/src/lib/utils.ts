2258196fa35c0e2903df0d97f9edf4757fa93868
Penelope Yong <penelopeysm@gmail.com>
2026-08-16 16:27:12 +0100

    Update website

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -6,12 +6,14 @@ const AD_BACKEND_ORDER: Record<string, number> = {
     /* Forward mode */
     ForwardDiff: 11,
     EnzymeFwd: 12,
-    MooncakeFwd: 13,
+    DifferFwd: 13,
+    MooncakeFwd: 14,
     /* Reverse mode */
     ReverseDiff: 21,
     ReverseDiffCompiled: 22,
     EnzymeRev: 23,
-    MooncakeRev: 24,
+    DifferRev: 24,
+    MooncakeRev: 25,
 };
 
 function compareADBackends(a: string, b: string): number {
